145ade34bb9bfd8eabdf7dcf471750f898ae0ae9
David A. Lévy <gDavidLevy@gmail.com>
2018-09-21 12:19:22 +0300

    update

--- src/main/java/ru/davidlevi/mysql/MainClass.java
+++ src/main/java/ru/davidlevi/mysql/MainClass.java
@@ -6,5 +6,38 @@ public class MainClass {
     public static void main(String[] args) throws Exception {
         MysqlConnector dao = new MysqlConnector();
         dao.readData();
+        /*
+        The connection is established
+
+        User: David
+        Website: https://davidlevi.ru
+        summary: Summary
+        Date: 2018-09-18
+        Comment: My first comment
+
+        User: David
+        Website: https://davidlevi.ru
+        summary: Summary
+        Date: 2018-09-18
+        Comment: My first comment
+
+        User: Test
+        Website: TestWebpage
+        summary: TestSummary
+        Date: 3910-01-10
+        Comment: TestComment
+
+        The columns in the table are:
+        Table: comments
+        Column 1 id
+        Column 2 MYUSER
+        Column 3 EMAIL
+        Column 4 WEBPAGE
+        Column 5 DATUM
+        Column 6 SUMMARY
+        Column 7 COMMENTS
+
+        Process finished with exit code 0
+         */
     }
 }
